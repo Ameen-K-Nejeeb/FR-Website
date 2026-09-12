458dcfb032a1ebe329fe973a7997e432af065cf3
Ameen K Nejeeb <ameenameen8701@gmail.com>
2026-09-12 16:40:01 +0530

    Configure production Cloudinary storage

--- config/settings.py
+++ config/settings.py
@@ -17,8 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     ".vercel.app",
